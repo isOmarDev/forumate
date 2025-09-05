edcd4c025cfbf741f43f46b97288702fbb932c56
omar kamel <isomardev@gmail.com>
2025-09-05 18:37:27 +0300

    refactor(env): rename API_URL → APP_API_URL and update env parsing

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,8 +2,8 @@ import * as z from 'zod';
 
 const createEnv = () => {
   const EnvSchema = z.object({
-    API_URL: z.string(),
-    ENABLE_API_MOCKING: z
+    APP_API_URL: z.string(),
+    APP_ENABLE_API_MOCKING: z
       .string()
       .refine((s) => s === 'true' || s === 'false')
       .transform((s) => s === 'true')
@@ -16,8 +16,8 @@ const createEnv = () => {
     Record<string, string>
   >((acc, curr) => {
     const [key, value] = curr;
-    if (key.startsWith('VITE_APP_')) {
-      acc[key.replace('VITE_APP_', '')] = value;
+    if (key.startsWith('VITE_')) {
+      acc[key.replace('VITE_', '')] = value;
     }
     return acc;
   }, {});
